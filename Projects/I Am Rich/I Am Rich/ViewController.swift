db09f608dcd025b707f0e340d1ef58b1ae78ca83
Calvin Wong <calwong42991@gmail.com>
2019-02-22 18:07:58 -0800

    I am Rich project

---
+++ Projects/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Calvin Wong on 2/22/19.
+//  Copyright © 2019 Calvin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
