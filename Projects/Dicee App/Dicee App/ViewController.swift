6b74d7e3c62095a75a0170ee072b5e949accd1b2
Calvin Wong <calwong42991@gmail.com>
2019-02-24 18:58:17 -0800

    Started Dicee App

---
+++ Projects/Dicee App/Dicee App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee App
+//
+//  Created by Calvin Wong on 2/24/19.
+//  Copyright © 2019 Calvin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
