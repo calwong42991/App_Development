c2c70b959c2c960998e4abf8b7024874165aa008
Calvin Wong <calwong42991@gmail.com>
2019-02-25 21:35:33 -0800

    declared dice array and changed dice image view dynamically

--- Projects/Dicee App/Dicee App/ViewController.swift
+++ Projects/Dicee App/Dicee App/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1 : UIImageView!
+    @IBOutlet weak var diceImageView2 : UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +27,9 @@ class ViewController: UIViewController {
         
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1]);
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2]);
         
     }
     
